1e4bf12f1b17f045a2a61ffbde4d51df6719842a
mmb78 <62362216+mmb78@users.noreply.github.com>
2026-04-15 19:01:46 +0200

    Add files via upload

---
+++ Info.lua
@@ -0,0 +1,11 @@
+return {
+    LrSdkVersion = 8.0,
+    LrToolkitIdentifier = 'com.yourname.aiannotate',
+    LrPluginName = 'Local AI Annotator',
+    LrLibraryMenuItems = {
+        {
+            title = "Generate AI Metadata",
+            file = "AIAnnotate.lua",
+        }
+    }
+}
